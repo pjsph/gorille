95f8f9eb9adc8bcabd7ed9fbd9fd18dbece94677
pjsph <pierrepangames@gmail.com>
2023-01-14 19:21:30 +0100

    Premier test de goroutines

--- gorille.go
+++ gorille.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+    "sync"
 
 	pkg "github.com/DylanMeeus/GoAudio/wave"
 )
@@ -13,11 +14,28 @@ var (
 	amp    = flag.Float64("a", 1.0, "amp mod factor")
 )
 
+func Map[T, U any](ts []T, f func(T) U) []U {
+    us := make([]U, len(ts))
+    for i := range ts {
+        us[i] = f(ts[i])
+    }
+    return us
+}
+
+var res []pkg.Frame
+
+func min(a, b int) int {
+    if a < b {
+        return a
+    }
+    return b
+}
+
 func main() {
 	fmt.Println("Parsing wave file..")
 	flag.Parse()
 	infile := *input
-	//outfile := *output
+	outfile := *output
 	scale := *amp
 	wave, err := pkg.ReadWaveFile(infile)
 	if err != nil {
@@ -26,22 +44,30 @@ func main() {
 
 	fmt.Printf("Read %v samples\n", len(wave.Frames))
 
-	// now try to write this file
-    for i, s := range wave.Frames {
-        ch := make(chan struct {int; pkg.Frame})
-        go changeAmplitude(ch, i, s, scale)
-        res := <- ch
-        fmt.Println(res.int, res.Frame)
+    var wg sync.WaitGroup
+
+    res = make([]pkg.Frame, len(wave.Frames))
+    var ires int = 0
+    for i := 0; i < len(wave.Frames); i += 9 {
+        var to_compute []pkg.Frame = wave.Frames[i:min(i+9, len(wave.Frames) - 1)]
+        wg.Add(1)
+        go changeAmplitude(i, to_compute, scale, &wg)
+        ires += min(9, len(wave.Frames)-i)
     }
 
-	//scaledSamples := changeAmplitude(wave.Frames, scale)
-	//if err := pkg.WriteFrames(scaledSamples, wave.WaveFmt, outfile); err != nil {
-    //	panic(err)
-	//}
+    wg.Wait()
+    fmt.Println("Finished, computed", ires, "samples")
+
+	if err := pkg.WriteFrames(res, wave.WaveFmt, outfile); err != nil {
+    	panic(err)
+	}
 
 	fmt.Println("done")
 }
 
-func changeAmplitude(ch chan struct {int; pkg.Frame}, i int, sample pkg.Frame, scalefactor float64) {
-        ch <- struct {int; pkg.Frame}{i, pkg.Frame(float64(sample) * scalefactor)}
+func changeAmplitude(startIndex int, samples []pkg.Frame, scalefactor float64, wg *sync.WaitGroup) {
+    for i, s := range samples {
+        res[startIndex + i] = pkg.Frame(float64(s) * scalefactor)
+    }
+    wg.Done()
 }
